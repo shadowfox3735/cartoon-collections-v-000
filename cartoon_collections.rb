2c9831c5b298335feebc0d7fdb3ee64f224519dd
shadowfox3735 <normanpan180@gmail.com>
2018-11-12 18:09:05 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -9,10 +9,10 @@ def summon_captain_planet(array)
 end
 
 def long_planeteer_calls(array)
-  array.any?( |word| word.length > 4)
+  array.any?{ |word| word.length > 4}
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(array)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
